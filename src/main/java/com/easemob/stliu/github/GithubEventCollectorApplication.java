dc3fcbb2c8b447d8ba72026bb77dbccbe35a4dbc
Strong Liu <stliu@hibernate.org>
2017-03-17 16:43:59 +0800

    initial commit

---
+++ src/main/java/com/easemob/stliu/github/GithubEventCollectorApplication.java
@@ -0,0 +1,12 @@
+package com.easemob.stliu.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubEventCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubEventCollectorApplication.class, args);
+	}
+}
